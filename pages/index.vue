8c002b345ba862cb76aa8500a98e888030bf4682
NikosKouroumpetsis <kourou67s@gmail.com>
2023-09-08 10:18:50 +0300

    scrollarea delete

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,17 @@
 <template>
   <div class="flex flex-col justify-center items-center min-h-screen">
     <div class="mb-12 flex justify-center">Δοκιμή components</div>
-    
   </div>
 </template>
 
 <script setup lang="ts">
-
-
-const tags = Array.from({ length: 50 }).map((_, i, a) => `v1.2.0-beta.${a.length - i}`)
+const tags = Array.from({ length: 50 }).map(
+  (_, i, a) => `v1.2.0-beta.${a.length - i}`
+);
 
 watchEffect(() => {
   console.log();
 });
 </script>
 
-<style>
-
-
-</style>
+<style></style>
